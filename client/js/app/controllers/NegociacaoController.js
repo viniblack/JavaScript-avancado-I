995e0f2d763270347d53fa2d20d5718270abae8b
Vini Black <viniibl4ck@gmail.com>
2020-03-17 21:09:45 -0300

    capturando os valores dos campos e melhorando um pouco a sintaxe do codigo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,15 @@
 class NegociacaoController{
 
   adiciona(event) {
-    event.preventDefault();
-    alert('Chamei ação no controller')
-  }
 
+    event.preventDefault();
+    let $ = document.querySelector.bind(document);
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+
+    console.log(inputData.value);
+    console.log(inputQuantidade.value);
+    console.log(inputValor.value);
+  }
 }
