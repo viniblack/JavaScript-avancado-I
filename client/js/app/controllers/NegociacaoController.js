97063d6a0801f759dbf51c3c87e045af7e8fef76
Vini Black <viniibl4ck@gmail.com>
2020-03-17 23:53:23 -0300

    transformando a data de string para date e deixando o codigo menos verboso

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,20 +1,30 @@
-class NegociacaoController{
+class NegociacaoController {
 
   constructor() {
 
     let $ = document.querySelector.bind(document);
-    
-    this.inputData = $('#data');
-    this.inputQuantidade = $('#quantidade');
-    this.inputValor = $('#valor');
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
   }
 
   adiciona(event) {
 
     event.preventDefault();
 
-    console.log(this.inputData.value);
-    console.log(this.inputQuantidade.value);
-    console.log(this.inputValor.value);
+    let data = new Date(
+      ...this._inputData.value
+        .split('-')
+        .map((item, indice) => item - indice % 2)
+    );
+    
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
+    
   }
 }
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,6 +1,7 @@
 class Negociacao {
 
   constructor(data, quantidade, valor) {
+    
     this._data = new Date(data.getTime());
     this._quantidade = quantidade;
     this._valor = valor;
