272514a4806a933353027654f80726cc9094e9b3
Black <viniibl4ck@gmail.com>
2020-03-31 18:09:33 -0300

    Isolando a responsabilidade de conversão de datas no DateHelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,24 +11,19 @@ class NegociacaoController {
     adiciona(event) {
         
         event.preventDefault();
-      
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-            
+        
+        let helper = new DateHelper();
+        
+
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value  
         );
-        
-        let diaMesAno = negociacao.data.getDate()
-        + '/' + (negociacao.data.getMonth() +1)
-        + '/' + negociacao.data.getFullYear();
+            
+        console.log(negociacao);
 
-
-        console.log(diaMesAno);
+        console.log(helper.dataParaTexto(negociacao.data));
     }
+
 }
