17080d2b9cb787cba98e2a6a8628e75da59de6c4
Black <viniibl4ck@gmail.com>
2020-04-01 10:03:56 -0300

    melhorando a formatação da data

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -14,7 +14,7 @@ class DateHelper {
 
  static textoParaData(texto) {
 
-  if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formato aaa-mm-dd');
+  if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Deve estar no formato aaa-mm-dd');
   return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 
  }
