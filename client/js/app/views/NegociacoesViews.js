8312e97af42e0376f3729a47e5bdab3776a14c90
Black <viniibl4ck@gmail.com>
2020-04-08 13:25:39 -0300

    agora quando inserimos algum dado ele cria a tabela

--- client/js/app/views/NegociacoesViews.js
+++ client/js/app/views/NegociacoesViews.js
@@ -4,7 +4,7 @@ class NegociacoesView {
 
   this._elemento = elemento;
  }
- _template() {
+ _template(model) {
 
   return `
   <table class="table table-hover table-bordered">
@@ -18,15 +18,24 @@ class NegociacoesView {
    </thead>
 
    <tbody>
+   ${model.negociacoes.map(n => `
    
+    <tr>
+     <td>${DateHelper.dataParaTexto(n.data)}</td>
+     <td>${n.quantidade}</td>
+     <td>${n.valor}</td>
+     <td>${n.volume}</td>
+    </tr>
+
+   `).join('')}
    </tbody>
   </table>
       `;
     }
 
-   update() {
+   update(model) {
 
-      this._elemento.innerHTML = this._template();
+      this._elemento.innerHTML = this._template(model);
 }
 }
 
