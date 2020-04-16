9d53379d77df689ed755553e2a23d22755b7439f
Black <viniibl4ck@gmail.com>
2020-04-16 14:06:42 -0300

    agr o dev não precisa lembrar de chamar o update para atualizar algum elemento da tela

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,8 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -20,8 +21,8 @@ class NegociacaoController {
     adiciona(event) {
         
         event.preventDefault();
+
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -31,7 +32,6 @@ class NegociacaoController {
 
     apaga() {
         this._listaNegociacoes.esvaziar();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
