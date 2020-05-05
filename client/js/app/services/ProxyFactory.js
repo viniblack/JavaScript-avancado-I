337945e994cfb7d572570b906dd888a7082ffd65
Vini Black <viniibl4ck@gmail.com>
2020-05-05 17:32:27 -0300

    arrumando o proxy pois ele só estava recebendo metodo, agora ele recebe propriedade tambem

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -1,21 +1,35 @@
 class ProxyFactory {
 
  static create(objeto, props, acao) {
+  return new Proxy(objeto, {
+    get(target, prop, receiver) {
+     if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
 
-     return new Proxy(objeto, {
+      return function() {
 
-         get(target, prop, receiver) {
+       console.log(`a propriedade "${prop}" foi interceptada`);
+       Reflect.apply(target[prop], target, arguments);
 
-             if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
-                 return function() {
-
-                     console.log(`a propriedade "${prop}" foi interceptada`);
-                     Reflect.apply(target[prop], target, arguments);
-                     return acao(target);
-                 }
-             }
-             return Reflect.get(target, prop, receiver);       
-         }
-     })
+       return acao(target);
+      }
+     }
+     return Reflect.get(target, prop, receiver);       
+    },
+    
+    set(target, prop, value, receiver) {
+     if(props.includes(prop)) {
+      target[prop] = value;
+      acao(target);
+     }
+    
+     return Reflect.set(target, prop, value, receiver);
+   }
+  })
  }
+
+ static _ehFuncao(func) {
+
+  return typeof(func) == typeof(Function);
+
+}
 }
