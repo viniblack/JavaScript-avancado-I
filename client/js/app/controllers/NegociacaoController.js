1ebd8fcf32ab84437624de75c9e5a9950dde7bee
Vini Black <viniibl4ck@gmail.com>
2020-06-29 15:37:37 -0300

    subindo pela metade acho

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -24,17 +24,19 @@ class NegociacaoController {
     importaNegociacoes() {
 
         let service = new NegociacaoService();
-      
-        service.obterNegociacoesDaSemana((erro,negociacoes) => {
-            if(erro) {
-                this._mensagem.texto = erro;
-                return;
-            }
-      
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
-        });
-      }
+        Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes => {
+            negociacoes
+            .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações da semana obtidas com sucesso';
+        })
+            .catch(erro => this._mensagem.texto = erro)
+
+    }
 
     apaga() {
 
