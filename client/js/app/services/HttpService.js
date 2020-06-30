2f2c0d5a156931d41128216703c879b8fd74f688
Vini Black <viniibl4ck@gmail.com>
2020-06-30 17:27:20 -0300

    sei la

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -11,11 +11,10 @@ class HttpService {
           if (xhr.status == 200) {
             resolve(JSON.parse(xhr.responseText));
           } else {
-            console.log(xhr.responseText);
             reject(xhr.responseText);
           }
         }
-      }
+      };
 
       xhr.send();
     });
--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,22 +1,26 @@
 class NegociacaoService {
 
     constructor() {
-        this.http = new HttpService();
+
+        this._http = new HttpService();
     }
 
-    obterNegociacoesDaSemana() {
+    obterNegociacoes() {
 
-        return new Promise((resolve, reject) => {
+        return Promise.all([
+            this.obterNegociacoesDaSemana(),
+            this.obterNegociacoesDaSemanaAnterior(),
+            this.obterNegociacoesDaSemanaRetrasada()
+        ]).then(periodos => {
 
-            this.http
-                .get('negociacoes/semana')
-                .then(negociacoes => {
-                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-                })
-                .catch(erro => {
-                    console.log(erro);
-                    reject('Não foi possível obter as negociações da semana');
-                })
+            let negociacoes = periodos
+                .reduce((dados, periodo) => dados.concat(periodo), []);
+
+            return negociacoes;
+
+        }).catch(erro => {
+            throw new Error(erro);
         });
+
     }
 }
