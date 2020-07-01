7c848a80271ee2a98f90fc2ad1c09dab82496898
Vini Black <viniibl4ck@gmail.com>
2020-07-01 16:44:30 -0300

    terminei o curso, mas estou fazendo umas melhorias

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,45 +11,39 @@ class NegociacaoController {
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia');
+            'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
 
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
             'texto');
+
+        this._ordemAtual = ''
     }
 
     adiciona(event) {
 
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._limpaFormulario();
-    }
-    
-        importaNegociacoes() {
-    
-    
-            let service = new NegociacaoService();
-            service
-                .obterNegociacoes()
-                .then(negociacoes => negociacoes.forEach(negociacao => {
-                    this._listaNegociacoes.adiciona(negociacao);
-                    this._mensagem.texto = 'Negociações do período importadas'
-                }))
-                .catch(erro => this._mensagem.texto = erro);
+        try {
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação adicionada com sucesso';
+            this._limpaFormulario();
+        } catch (erro) {
+            this._mensagem.texto = erro;
         }
+    }
 
-/*     importaNegociacoes() {
+    importaNegociacoes() {
+
 
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
-            .then(negociacoes => {
-                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-                this._mensagem.texto = 'Negociações do período importadas com sucesso';
-            })
-            .catch(error => this._mensagem.texto = error);
-    } */
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas'
+            }))
+            .catch(erro => this._mensagem.texto = erro);
+    }
 
     apaga() {
 
@@ -71,5 +65,14 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+
+    ordena(coluna) {
+        if (this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
     }
 }
